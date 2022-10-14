f0ee089b24ab524bf50698c329412d2aa4a7c6f2
Vicky Kumar Singh <vickysinghhere@gmail.com>
2022-10-14 17:13:40 +0530

    update

---
+++ src/main/java/com/vickysingh/dashboard/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.vickysingh.dashboard.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Azure Spring Apps!";
+    }
+
+}
